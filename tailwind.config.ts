f2d710779bb0faf29545c6f40e6a1fb5263a07be
Marcelo Rodriguez <maarodrii@gmail.com>
2025-07-12 00:26:02 +0000

    Elige un texto legible para el proyecto, noto que no se ve realmente bie

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Lora', 'serif'],
-        headline: ['Lora', 'serif'],
+        body: ['Inter', 'sans-serif'],
+        headline: ['Inter', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
