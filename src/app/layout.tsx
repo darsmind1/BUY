6f867f7b7aa264319e73191be19cdeb6852bafbd
Marcelo Rodriguez <maarodrii@gmail.com>
2025-07-12 00:19:25 +0000

    Intenta elegir una fuente legible, estilo times new roman

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,9 +17,9 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body font-thin antialiased">
+      <body className="font-body font-normal antialiased">
         {children}
         <Toaster />
       </body>
